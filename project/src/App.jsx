76ba464fba73d20d9e5babb13316774b08d2e887
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-06-03 22:04:45 +0900

    react rendering방법

---
+++ project/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header>hello</header>
+      <main>main</main>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
+
+// 1. App함수는 id가 root인 div안에 저 html태그들을 넣어주는구나..! 
+// => 라고 일단은 생각할 수 있음 
